3d55ed538a8f0827aee60b3b9411078b103345c4
kitsensores <kitsensores.ifg@gmail.com>
2022-10-21 15:05:07 -0300

    Codigo do arduino

---
+++ RELEY.ino
@@ -0,0 +1,14 @@
+int relayPin = 2;    
+
+void setup()
+{
+pinMode(relayPin, OUTPUT);     
+}
+
+void loop()
+{
+digitalWrite(relayPin, HIGH);       
+delay(1000);
+digitalWrite(relayPin,LOW);        
+delay(1000);
+
